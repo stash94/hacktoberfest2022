1f546bbcab0c84a51d9d567223c9161b04300fc0
Nicholas Farley <nicholas_farley@colpal.com>
2019-05-28 17:06:04 -0400

    Add calculator program

---
+++ bin.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+const program = require('commander')
+
+program
+  .command('* <a> <b> [others...]')
+  .action((a, b, others) => {
+    console.log([a, b, ...others]
+      .reduce((acc, cur) => acc * cur))
+  })
+
+program
+  .command('/ <a> <b> [others...]')
+  .action((a, b, others) => {
+    console.log([a, b, ...others]
+      .reduce((acc, cur) => acc / cur))
+  })
+
+program
+  .command('+ <a> <b> [others...]')
+  .action((a, b, others) => {
+    console.log([a, b, ...others]
+      .reduce((acc, cur) => acc + cur))
+  })
+
+program
+  .command('- <a> <b> [others...]')
+  .action((a, b, others) => {
+    console.log([a, b, ...others]
+      .reduce((acc, cur) => acc - cur))
+  })
+
+program.parse(process.argv)
