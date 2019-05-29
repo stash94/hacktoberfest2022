7cc8cc7375a687f5a314eb84932bb1354c43278c
Jenna Bousellam <jenna_bousellam@colpal.com>
2019-05-29 15:45:23 -0400

    Fix broken addition function

--- calculator.js
+++ calculator.js
@@ -19,7 +19,7 @@ program
 program
   .command  ('+ <a> <b> [others...]')
   .action((a, b, others) => {
-    console.log([a, b, ...others].reduce((acc, cur) => { return acc + cur }));
+    console.log([a, b, ...others].reduce((acc, cur) => { return parseInt(parseInt(acc) + parseInt(cur))}));
   });
 
 program
