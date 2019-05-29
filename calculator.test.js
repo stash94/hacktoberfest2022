60b59134a3f982655aec8af78c9f5d9448dced92
Jenna Bousellam <jenna_bousellam@colpal.com>
2019-05-29 15:45:52 -0400

    Add tests for subtraction multiplication division

--- calculator.test.js
+++ calculator.test.js
@@ -6,3 +6,18 @@ test('addition', () => {
   const actual = run(['+', 3, 4]);
   expect(actual).toBe(7);
 });
+
+test('subtraction', () => {
+  const actual = run(['-', 15, 4]);
+  expect(actual).toBe(11);
+});
+
+test('muplication', () => {
+  const actual = run(['x', 3, 4]);
+  expect(actual).toBe(12);
+});
+
+test('division', () => {
+  const actual = run(['/', 10, 5]);
+  expect(actual).toBe(2);
+});
