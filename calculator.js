641e03a14db3ab7cff6c7e504de7d95dd449c03a
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-06 13:08:23 -0500

    Fix lint errors

--- calculator.js
+++ calculator.js
@@ -2,34 +2,27 @@
 const program = require('commander');
 
 program
-.command('x <a> <b> [others...]')
+  .command('x <a> <b> [others...]')
   .action((a, b, others) => {
-    console.log([
-      
-      
-      a, b, ...others].reduce((acc, cur) => acc * cur));
-      });
+    console.log([a, b, ...others].reduce((acc, cur) => acc * cur));
+  });
 
 program
-
   .command('/ <a> <b> [others...]')
-
-  .action((a, b,   others) => {
+  .action((a, b, others) => {
     console.log([a, b, ...others].reduce((acc, cur) => acc / cur));
   });
 
 program
-  .command  ('+ <a> <b> [others...]')
-  .action((a, b, others)=>{
-    console.log([a, b, ...others].reduce((acc, cur) => { return acc + cur }));
+  .command('+ <a> <b> [others...]')
+  .action((a, b, others) => {
+    console.log([a, b, ...others].reduce((acc, cur) => acc + cur));
   });
 
 program
   .command('- <a> <b> [others...]')
-  .action((
-a, b, others) => {
-    console.log([a, b, 
-    ...others].reduce((acc, cur) => acc - cur))
-  })
+  .action((a, b, others) => {
+    console.log([a, b, ...others].reduce((acc, cur) => acc - cur));
+  });
 
 program.parse(process.argv);
