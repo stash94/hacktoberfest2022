61aabe68dfd77b860071397855c59cff339273cb
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-06 13:33:33 -0500

    Add missing tests

--- calculator.test.js
+++ calculator.test.js
@@ -2,7 +2,22 @@ const { execFileSync } = require('child_process');
 
 const run = (...args) => parseFloat(execFileSync('./calculator.js', args, { encoding: 'utf8' }));
 
+test('multiplication', () => {
+  const actual = run('x', 3, 4);
+  expect(actual).toBe(12);
+});
+
+test('division', () => {
+  const actual = run('/', 3, 4);
+  expect(actual).toBe(0.75);
+});
+
 test('addition', () => {
   const actual = run('+', 3, 4);
   expect(actual).toBe(7);
+});
+
+test('subtraction', () => {
+  const actual = run('-', 3, 4);
+  expect(actual).toBe(-1);
 });
