c00901d249180b54268edf3c9ef18ef731d3f149
Nicholas Farley <nicholasafarley@gmail.com>
2020-01-06 12:46:07 -0500

    Simplify usage of "run" helper fn

--- calculator.test.js
+++ calculator.test.js
@@ -1,8 +1,8 @@
 const { execFileSync } = require('child_process');
 
-const run = args => parseFloat(execFileSync('./calculator.js', args, { encoding: 'utf8' }));
+const run = (...args) => parseFloat(execFileSync('./calculator.js', args, { encoding: 'utf8' }));
 
 test('addition', () => {
-  const actual = run(['+', 3, 4]);
+  const actual = run('+', 3, 4);
   expect(actual).toBe(7);
 });
